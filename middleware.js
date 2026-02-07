fc5e848a8ebb2d798e4aa2c512c13e753df6551b
Claw <khalid@wellplan.io>
2026-02-07 00:37:24 +0000

    Test: localePrefix always + explicit matcher

--- middleware.js
+++ middleware.js
@@ -3,9 +3,9 @@ import createMiddleware from 'next-intl/middleware';
 export default createMiddleware({
   locales: ['en', 'nl'],
   defaultLocale: 'en',
-  localePrefix: 'as-needed'
+  localePrefix: 'always'
 });
 
 export const config = {
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
+  matcher: ['/', '/(en|nl)/:path*']
 };
